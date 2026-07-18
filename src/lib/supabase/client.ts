873b76c4a7607158e2d3e0c3f632001b6afe6548
ahadhasan32 <bnsq37@gmail.com>
2026-07-18 23:32:59 +0600

    Update client.ts

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,1 +1,20 @@
+import { createBrowserClient } from "@supabase/ssr";
 
+export function createClient() {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+  if (!supabaseUrl) {
+    throw new Error(
+      "Missing NEXT_PUBLIC_SUPABASE_URL in the .env.local file."
+    );
+  }
+
+  if (!supabaseAnonKey) {
+    throw new Error(
+      "Missing NEXT_PUBLIC_SUPABASE_ANON_KEY in the .env.local file."
+    );
+  }
+
+  return createBrowserClient(supabaseUrl, supabaseAnonKey);
+}
